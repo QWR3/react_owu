e1f71378bbc3892db14d2acba9611183a3b997b0
QWR3 <anton7@i.ua>
2020-12-07 17:23:21 +0200

    2

---
+++ src/posts/Posts.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Post from "./post/Post";
+
+class Posts extends Component {
+    state ={posts:[] ,choosen:null}
+    choosenOne=(id)=>{
+        let postsF =this.state.posts
+        let find =postsF.find(value => value.id ===id)
+        this.setState({choosen:find})
+    }
+    render() {
+        let {posts , choosen}=this.state
+
+        return (
+            <div>
+                {
+                    // console.log(this.state)
+                    posts.map((value, index) =>
+                        <Post post={value} ChoosenOne={this.choosenOne} key={index}/>
+                     )
+                }
+                {
+                    choosen &&<h2>{choosen.id}-{choosen.title}</h2>
+                }
+            </div>
+        );
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(value => {
+                return value.json()
+            })
+            .then(posts => {
+                console.log(posts);
+                this.setState({posts})
+            })
+    }
+}
+
+export default Posts;
---
+++ src/posts/post/Post.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+    render() {
+        let {post , ChoosenOne} =this.props
+        let title =post.title
+        return (
+            <div>
+            {post.id}-{post.title}
+            <button onClick={()=>ChoosenOne(post.id)}> вибрати</button>
+            </div>
+        );
+    }
+}
+
+export default Post;
